6cf092a611705440b4d6d17cf22fa543b1da2fd1
Michael Ray-Von <michaelrayvon@gmail.com>
2023-11-13 20:25:05 +0100

    fix rand return val

--- pages/api/getRands.ts
+++ pages/api/getRands.ts
@@ -24,10 +24,12 @@ export default async function handler(
   }
   try {
     const qrnd: {
-      type: string;
-      length: number;
-      data: number[];
-      success: boolean;
+      data: {
+        type: string;
+        length: number;
+        data: number[];
+        success: boolean;
+      };
     } = await axios.get("https://api.quantumnumbers.anu.edu.au", {
       headers: {
         "x-api-key": process.env.NEXT_PUBLIC_QRNG_API_KEY,
@@ -39,13 +41,13 @@ export default async function handler(
       timeout: 2000,
     });
     res.status(200).json({
-      rands: qrnd.data,
+      rands: qrnd.data.data,
       message: messages.quantum,
     });
   } catch (e: any) {
     res.status(200).json({
       rands: Array.from(randomBytes(1024).values()),
-      message: messages.pseudo,
+      message: `error: ${e.message}. ${messages.pseudo}`,
     });
   }
 }
